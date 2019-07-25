ca6c3241ff81597f25cbd7cd22915ef839cfdb1f
vitaminvp <vitamin@ukr.net>
2019-07-25 10:48:23 +0300

    add createStore

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@
 // // unregister() to register() below. Note this comes with some pitfalls.
 // // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
-import { createStore } from "redux";
+
+//import { createStore } from "redux";
 
 const ACTIONS_TYPE = {
   INCREMENT: "INCREMENT",
@@ -28,8 +29,43 @@ const counter = (state = 0, action) => {
   }
 };
 
+const createStore = reducer => {
+  let store;
+  let listeners = [];
+
+  const getState = () => store;
+
+  const dispatch = action => {
+    store = reducer(store, action);
+    listeners.forEach(listener => listener());
+  };
+
+  dispatch({});
+
+  const subscribe = listener => {
+    listeners.push(listener);
+
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    };
+  };
+
+  return { getState, dispatch, subscribe };
+};
+
 const store = createStore(counter);
 console.log(store.getState());
 
-store.dispatch({ type: ACTIONS_TYPE.INCREMENT });
-console.log(store.getState());
+// store.dispatch({ type: ACTIONS_TYPE.INCREMENT });
+// console.log(store.getState());
+
+const render = () => {
+  document.body.innerHTML = store.getState();
+};
+
+render();
+store.subscribe(render);
+
+document.addEventListener("click", () => {
+  store.dispatch({ type: ACTIONS_TYPE.INCREMENT });
+});
