2c8930d4e8331c5ecb5f7f35cce1bc3700198468
vitaminvp <vitamin@ukr.net>
2019-07-26 12:26:27 +0300

    Updates

---
+++ precache-manifest.dd2c5280e1836726c649e6f9408fd77d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2466f958eb18989b689414ea965207",
+    "url": "/Getting-started-with-redux/index.html"
+  },
+  {
+    "revision": "e5a9d44172fba5dbb250",
+    "url": "/Getting-started-with-redux/static/js/2.f66fcbdb.chunk.js"
+  },
+  {
+    "revision": "66edf85ce74f9c0f5920",
+    "url": "/Getting-started-with-redux/static/js/main.1f99c066.chunk.js"
+  },
+  {
+    "revision": "ccc5050848d32b178c13",
+    "url": "/Getting-started-with-redux/static/js/runtime~main.06875eb5.js"
+  }
+]);
